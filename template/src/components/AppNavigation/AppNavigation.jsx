da37d5d89ad7fb12379e7199941cad2406117179
Nickslabcode <nicknenovski@gmail.com>
2024-08-01 22:15:27 +0300

    logout logic implemented

--- template/src/components/AppNavigation/AppNavigation.jsx
+++ template/src/components/AppNavigation/AppNavigation.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import Button from '../../hoc/Button/Button';
+import { useAuth } from '../../providers/AuthProvider.jsx';
 import Logo from '../Logo/Logo';
 import Search from '../Search/Search';
 import ToggleTheme from '../ToggleTheme/ToggleTheme';
 import UserMenu from '../UserMenu/UserMenu';
 
 function AppNavigation() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  }
+
   return (
     <div>
       <Logo />
@@ -14,6 +24,7 @@ function AppNavigation() {
       </Button>
       <ToggleTheme />
       <UserMenu />
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
