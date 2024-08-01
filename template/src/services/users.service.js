c9713b1bc09c2b52671d410bad8bd7fa55347e50
Maikul <mcjaiyo01@gmail.com>
2024-08-01 16:18:17 +0300

    implemented user.service

---
+++ template/src/services/users.service.js
@@ -0,0 +1,61 @@
+import { get, set, ref, query, equalTo, orderByChild } from 'firebase/database';
+import { db } from '../config/firebase.config';
+
+export const getAllUsers = async (search = '') => {
+  const snapshot = await get(ref(db, `users`));
+  if (!snapshot.exists()) return [];
+
+  const users = Object.values(snapshot.val());
+
+  if (search) {
+    return users.filter(user =>
+      user.username.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+  }
+
+  return users;
+};
+
+export const getUserByHandle = async handle => {
+  const snapshot = await get(ref(db, `users/${handle}`));
+  if (!snapshot.exists()) {
+    return new Error('User not found!');
+  }
+  return {
+    ...Object.values(snapshot.val()),
+    posts: Object.keys(snapshot.val().posts ?? {}),
+    comments: Object.keys(snapshot.val().comments ?? {}),
+    likedPosts: Object.keys(snapshot.val().likedPosts ?? {}),
+  };
+};
+
+export const createrUser = async (
+  username,
+  firstName,
+  lastName,
+  email,
+  password,
+  role = 'user',
+  isBLocked = false
+) => {
+  const user = {
+    username,
+    firstName,
+    lastName,
+    email,
+    password,
+    role,
+    isBLocked,
+  };
+  await set(ref(db, `users/${username}`, user));
+  await update(ref(db), {
+    [`users/${handle}/username`]: username,
+  });
+};
+
+export const changeUserDetails = (handle, target, value) => {
+  const updateObject = {
+    [`users/${handle}/${target}`]: value,
+  };
+  return update(ref(db, updateObject));
+};
