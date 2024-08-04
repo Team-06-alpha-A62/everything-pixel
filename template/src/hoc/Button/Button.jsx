d02525f6557ac17d7fc5d1f966f24334ba5332db
Nikola Nenovski <nicknenovski@gmail.com>
2024-08-04 15:02:44 +0300

    updated logic for the button hoc to include handleClick functions

--- template/src/hoc/Button/Button.jsx
+++ template/src/hoc/Button/Button.jsx
@@ -1,11 +1,12 @@
 import styles from './Button.module.scss';
 import PropTypes from 'prop-types';
-function Button({ style, children }) {
-  return <div className={styles[style]}>{children}</div>;
+function Button({ style, children, handleClick }) {
+  return <button onClick={handleClick} className={styles[style]}>{children}</button>;
 }
 
 Button.propTypes = {
   style: PropTypes.string.isRequired,
   children: PropTypes.any.isRequired,
+  handleClick: PropTypes.func
 };
 export default Button;
