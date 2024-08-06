cf444606dd716e645c4976ba6416325cc4e92568
Nikola Nenovski <nicknenovski@gmail.com>
2024-08-06 18:06:10 +0300

    Lottie integration and NotFound view implementation (was in the wrong directory)

--- template/src/views/NotFound/NotFound.jsx
+++ template/src/views/NotFound/NotFound.jsx
@@ -1,14 +1,22 @@
 import { useNavigate } from 'react-router-dom';
+import animationData from '../../assets/404-animation.json';
+import Lottie from 'lottie-react';
+import styles from './NotFound.module.scss';
 
 const NotFound = () => {
   const navigate = useNavigate();
 
-  return (
-      <div>
-        <h1>404 not found!</h1>
-        <button onClick={() => navigate(-1)}>&larr; Back</button>
-      </div>
-  )
-}
 
-export default NotFound
+  return (
+    <div className={styles['not-found-container']}>
+      <Lottie
+        animationData={animationData}
+        className={styles['lottie-animation']}
+      />
+      <p>Oops...Seems you&apos;re lost</p>
+      <button onClick={() => navigate(-1)}>&larr; Back</button>
+    </div>
+  );
+};
+
+export default NotFound;
