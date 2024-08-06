57433e2fcf31a08b5b456d988131dee87dcbfdd2
Maikul <mcjaiyo01@gmail.com>
2024-08-06 21:37:55 +0300

    created postCommentEnum + postReportEnum

---
+++ template/src/enums/postCommentEnum.jsx
@@ -0,0 +1,8 @@
+const postCommentEnum = {
+  SPAM: 'spam',
+  ABUSIVE_LANGUAGE: 'abusive language',
+  OFF_TOPIC: 'off topic',
+  HATE_SPEECH: 'hate speech',
+};
+
+export default postCommentEnum;
---
+++ template/src/enums/postReportEnum.jsx
@@ -0,0 +1,8 @@
+const postReportEnum = {
+  SPAM: 'spam',
+  MISINFORMATION: 'misinformation',
+  HARASSMENT: 'harassment',
+  INAPPROPRIATE_CONTENT: 'inappropriate content',
+};
+
+export default postReportEnum;
